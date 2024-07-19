be1d37813b7447f5124602098d62ecdab28d3708
Hassan Brahimi <hbrahimi@c1r4p2.1337.ma>
2024-07-19 10:41:45 +0100

    echo done

---
+++ builtins/echo.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbrahimi <hbrahimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 10:37:11 by hbrahimi          #+#    #+#             */
+/*   Updated: 2024/07/19 10:41:25 by hbrahimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+#include "../inc/builtins.h"
+
+void respond_to_echo(char *str, bool flag)
+{
+    if (flag)
+        printf("%s, str");
+    else
+        printf("%s\n");
+}
