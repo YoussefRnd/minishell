462d0a0bca43b39203caf3bfc1caddba01717e0e
Hassan Brahimi <hbrahimi@c1r4p2.1337.ma>
2024-07-19 11:43:50 +0100

    pwd

---
+++ builtins/src/pwd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbrahimi <hbrahimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/19 11:37:25 by hbrahimi          #+#    #+#             */
+/*   Updated: 2024/07/19 11:43:20 by hbrahimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+#include "../inc/builtins.h"
+
+void	respond_to_pwd(void)
+{
+	char cwd[1024];
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		printf("%s\n", cwd);
+	}
+	else
+	{
+		perror("getcwd() error");
+	}
+}
+
+// int main()
+// {
+// 	respond_to_pwd();
+// }
