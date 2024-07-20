abadb7fd590d4b57d2579444653f4a95d8d7a2c5
Hassan Brahimi <hbrahimi@c2r1p6.1337.ma>
2024-07-20 14:14:34 +0100

    added

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hbrahimi <hbrahimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:59:08 by yboumlak          #+#    #+#             */
-/*   Updated: 2024/07/19 13:13:40 by hbrahimi         ###   ########.fr       */
+/*   Updated: 2024/07/20 13:27:38 by hbrahimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,4 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
